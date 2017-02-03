6b0e8b62f7da593b89b222e39ccc69069410ec57
Glen Baker <iepathos@gmail.com>
2017-02-03 17:02:34 -0500

    Built for deployment

--- docs/js/email.js
+++ docs/js/email.js
@@ -4,5 +4,6 @@ $('#contact_form').on('submit', function(e) { //use on if jQuery 1.7+
     var subject = $('#subject').val();
     var body = $('#message').val();
     window.open('mailto:glen@ehab.it?subject='+subject+'&body='+body);
+    $('#reset').click();
     $('#contact div.close').click();
 });
