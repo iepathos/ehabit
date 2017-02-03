515ed574525088f4e3d24193f3a09902341eecce
Glen Baker <iepathos@gmail.com>
2017-02-03 17:00:46 -0500

    Added email javascript, improved contact form

---
+++ static/js/email.js
@@ -0,0 +1,8 @@
+
+$('#contact_form').on('submit', function(e) { //use on if jQuery 1.7+
+    e.preventDefault();  //prevent form from submitting
+    var subject = $('#subject').val();
+    var body = $('#message').val();
+    window.open('mailto:glen@ehab.it?subject='+subject+'&body='+body);
+    $('#contact div.close').click();
+});
