acb1b5cbfa95e4d7dc9c2ab0f8cc20ce730862b2
Glen Baker <iepathos@gmail.com>
2017-02-04 16:22:51 -0500

    Removed contact form and email javascript

--- static/js/email.js
+++
@@ -1,9 +0,0 @@
-
-$('#contact_form').on('submit', function(e) { //use on if jQuery 1.7+
-    e.preventDefault();  //prevent form from submitting
-    var subject = $('#subject').val();
-    var body = $('#message').val();
-    window.open('mailto:glen@ehab.it?subject='+subject+'&body='+body);
-    $('#reset').click();
-    $('#contact div.close').click();
-});
